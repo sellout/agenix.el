03eae2bdee9590494b97c00862cb8885e2dfeaa2
Greg Pfeil <greg@technomadic.org>
2024-11-09 17:24:06 -0700

    Update Bash strict mode
    - moves functions like `bash-strict-mode.lib.foo pkgs` to `bash-strict-mode.pkgsLib.${system}.foo`,
      which is accessible as `pkgs.foo` via the overlay;
    - switches `devShells` to use `shellcheck` (upstream Nixpkgs change was published); and
    - adds more comprehensive CI in garnix.

--- flake.nix
+++ flake.nix
@@ -11,6 +11,8 @@
 
     bash-strict-mode = {
       url = "github:sellout/bash-strict-mode";
+      ## TODO: Change this to `inputs.flaky.inputs.nixpkgs.follows` once NixOS/nix#5790 is fixed,
+      ##       because that will unify all of the transitive instances.
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -79,137 +81,127 @@
         # We need to tell Eldev where to find its Emacs package.
         ELDEV_LOCAL = emacsPath pkgs.emacsPackages.eldev;
       in {
-        _module.args.pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [(import ./nix/dependencies.nix)];
-        };
+        _module.args.pkgs = inputs.nixpkgs.legacyPackages.${system}.appendOverlays [
+          inputs.bash-strict-mode.overlays.default
+          (import ./nix/dependencies.nix)
+        ];
         packages = {
           default = self'.packages.agenix-el;
 
-          agenix-el =
-            inputs.bash-strict-mode.lib.checkedDrv pkgs
-            (pkgs.emacsPackages.trivialBuild {
-              inherit ELDEV_LOCAL src;
+          agenix-el = pkgs.checkedDrv (pkgs.emacsPackages.trivialBuild {
+            inherit ELDEV_LOCAL src;
 
-              pname = "agenix";
-              version = readVersion ./agenix.el;
-
-              nativeBuildInputs = [
-                pkgs.emacs
-                # Emacs-lisp build tool, https://doublep.github.io/eldev/
-                pkgs.emacsPackages.eldev
-              ];
-
-              doCheck = true;
-              checkPhase = ''
-                runHook preCheck
-                eldev test
-                runHook postCheck
-              '';
-
-              doInstallCheck = true;
-              instalCheckPhase = ''
-                runHook preInstallCheck
-                eldev --packaged test
-                runHook postInstallCheck
-              '';
-            });
-        };
-
-        devShells.default =
-          ## TODO: Use `inputs.bash-strict-mode.lib.checkedDrv` here after
-          ##       NixOS/nixpkgs#204606 makes it into a release.
-          inputs.bash-strict-mode.lib.drv pkgs
-          (pkgs.mkShell {
-            inputsFrom =
-              builtins.attrValues inputs.self.checks.${system}
-              ++ builtins.attrValues inputs.self.packages.${system};
+            pname = "agenix";
+            version = readVersion ./agenix.el;
 
             nativeBuildInputs = [
-              # Nix language server,
-              # https://github.com/oxalica/nil#readme
-              pkgs.nil
-              # Bash language server,
-              # https://github.com/bash-lsp/bash-language-server#readme
-              pkgs.nodePackages.bash-language-server
+              pkgs.emacs
+              # Emacs-lisp build tool, https://doublep.github.io/eldev/
+              pkgs.emacsPackages.eldev
             ];
+
+            doCheck = true;
+            checkPhase = ''
+              runHook preCheck
+              eldev test
+              runHook postCheck
+            '';
+
+            doInstallCheck = true;
+            instalCheckPhase = ''
+              runHook preInstallCheck
+              eldev --packaged test
+              runHook postInstallCheck
+            '';
           });
+        };
+
+        devShells.default = pkgs.checkedDrv (pkgs.mkShell {
+          inputsFrom =
+            builtins.attrValues inputs.self.checks.${system}
+            ++ builtins.attrValues inputs.self.packages.${system};
+
+          nativeBuildInputs = [
+            # Nix language server,
+            # https://github.com/oxalica/nil#readme
+            pkgs.nil
+            # Bash language server,
+            # https://github.com/bash-lsp/bash-language-server#readme
+            pkgs.nodePackages.bash-language-server
+          ];
+        });
 
         checks = {
-          eldev-doctor =
-            inputs.bash-strict-mode.lib.checkedDrv pkgs
-            (pkgs.stdenv.mkDerivation {
-              inherit ELDEV_LOCAL src;
+          eldev-doctor = pkgs.checkedDrv (pkgs.stdenv.mkDerivation {
+            inherit ELDEV_LOCAL src;
 
-              name = "eldev-doctor";
+            name = "eldev-doctor";
 
-              nativeBuildInputs = [
-                pkgs.emacs
-                # Emacs-lisp build tool, https://doublep.github.io/eldev/
-                pkgs.emacsPackages.eldev
-              ];
+            nativeBuildInputs = [
+              pkgs.emacs
+              # Emacs-lisp build tool, https://doublep.github.io/eldev/
+              pkgs.emacsPackages.eldev
+            ];
 
-              buildPhase = ''
-                runHook preBuild
-                ## TODO: Currently needed to make a temp file in
-                ##      `eldev--create-internal-pseudoarchive-descriptor`.
-                export HOME="$(mktemp --directory --tmpdir fake-home.XXXXXX)"
-                mkdir -p "$HOME/.cache/eldev"
-                eldev doctor
-                runHook postBuild
-              '';
+            buildPhase = ''
+              runHook preBuild
+              ## TODO: Currently needed to make a temp file in
+              ##      `eldev--create-internal-pseudoarchive-descriptor`.
+              export HOME="$(mktemp --directory --tmpdir fake-home.XXXXXX)"
+              mkdir -p "$HOME/.cache/eldev"
+              eldev doctor
+              runHook postBuild
+            '';
 
-              installPhase = ''
-                runHook preInstall
-                mkdir -p "$out"
-                runHook postInstall
-              '';
-            });
+            installPhase = ''
+              runHook preInstall
+              mkdir -p "$out"
+              runHook postInstall
+            '';
+          });
 
-          eldev-lint =
-            inputs.bash-strict-mode.lib.checkedDrv pkgs
-            (pkgs.stdenv.mkDerivation {
-              inherit ELDEV_LOCAL src;
+          eldev-lint = pkgs.checkedDrv (pkgs.stdenv.mkDerivation {
+            inherit ELDEV_LOCAL src;
 
-              name = "eldev-lint";
+            name = "eldev-lint";
 
-              nativeBuildInputs = [
-                pkgs.emacs
-                pkgs.emacsPackages.eldev
-              ];
+            nativeBuildInputs = [
+              pkgs.emacs
+              pkgs.emacsPackages.eldev
+            ];
 
-              postPatch = ''
-                { echo
-                  echo "(mapcar"
-                  echo " 'eldev-use-local-dependency"
-                  echo " '(\"${emacsPath pkgs.emacsPackages.dash}\""
-                  echo "   \"${emacsPath pkgs.emacsPackages.elisp-lint}\""
-                  echo "   \"${emacsPath pkgs.emacsPackages.package-lint}\""
-                  echo "   \"${emacsPath pkgs.emacsPackages.relint}\""
-                  echo "   \"${emacsPath pkgs.emacsPackages.xr}\"))"
-                } >> Eldev
-              '';
+            postPatch = ''
+              { echo
+                echo "(mapcar"
+                echo " 'eldev-use-local-dependency"
+                echo " '(\"${emacsPath pkgs.emacsPackages.dash}\""
+                echo "   \"${emacsPath pkgs.emacsPackages.elisp-lint}\""
+                echo "   \"${emacsPath pkgs.emacsPackages.package-lint}\""
+                echo "   \"${emacsPath pkgs.emacsPackages.relint}\""
+                echo "   \"${emacsPath pkgs.emacsPackages.xr}\"))"
+              } >> Eldev
+            '';
 
-              buildPhase = ''
-                runHook preBuild
-                ## TODO: Currently needed to make a temp file in
-                ##      `eldev--create-internal-pseudoarchive-descriptor`.
-                export HOME="$PWD/fake-home"
-                mkdir -p "$HOME"
-                ## NB: Need `--external` here so that we don’t try to download any
-                ##     package archives (which would break the sandbox).
-                ## TODO: Re-enable relint, currently it errors, I think because it
-                ##       or Eldev is expecting a multi-file package.
-                eldev --external lint elisp # re
-                runHook postBuild
-              '';
+            buildPhase = ''
+              runHook preBuild
+              ## TODO: Currently needed to make a temp file in
+              ##      `eldev--create-internal-pseudoarchive-descriptor`.
+              export HOME="$PWD/fake-home"
+              mkdir -p "$HOME"
+              ## NB: Need `--external` here so that we don’t try to download any
+              ##     package archives (which would break the sandbox).
+              ## TODO: Re-enable relint, currently it errors, I think because it
+              ##       or Eldev is expecting a multi-file package.
+              eldev --external lint elisp # re
+              runHook postBuild
+            '';
 
-              installPhase = ''
-                runHook preInstall
-                mkdir -p "$out"
-                runHook preInstall
-              '';
-            });
+            installPhase = ''
+              runHook preInstall
+              mkdir -p "$out"
+              runHook preInstall
+            '';
+          });
 
           nix-fmt = pkgs.stdenv.mkDerivation {
             inherit src;
